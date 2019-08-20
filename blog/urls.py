2b8013a02c55463560967c0c63a7b6be18fb11b6
hwang-yj <hyjok_wow@naver.com>
2019-08-20 11:31:48 +0900

    change map

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
+
     path('', views.a_view, name='a_view'),
     #path('upload/', views.upload, name='upload'),
     #path('', views.connection_test, name='connection_test'),
