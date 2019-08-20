294c948f9ccd149e9cec6a677ce1551acfe042b4
hwang-yj <hyjok_wow@naver.com>
2019-08-20 11:08:13 +0900

    change blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
-from django.conf.urls import include, url
+from django.conf.urls import url, include
+from blog import views
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 
-
-
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    
+    path('', views.a_view, name='a_view'),
+    #path('upload/', views.upload, name='upload'),
+    #path('', views.connection_test, name='connection_test'),
+    #path('upload/', views.upload, name='upload'),  #test
+    path('blog/', views.snippet_list),
+    path('blog/<int:pk>/', views.snippet_detail),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,13 +13,15 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 # Create your views here.
 
+'''
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'blog/post_list.html', {'posts': posts})
+'''
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def a_view(request):
+    return render(request, 'blog/post_list.html',{'time':datetime.now()})
+
 
 class JSONResponse(HttpResponse):
     """
